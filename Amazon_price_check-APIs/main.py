e0950737f8c64d1b8a93882415189e5b426ced2e
PerakaJahnavi <perakajahnavi01@gmail.com>
2023-02-06 19:26:24 +0530

    secret_keys

--- Amazon_price_check-APIs/main.py
+++ Amazon_price_check-APIs/main.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
-my_email = "panakaluperaka@gmail.com"
-password = "hyckjhinjkjzsrgn"
-to_me = "jahnaviperaka@gmail.com"
+my_email = ""
+password = ""
+to_me = ""
 headers = {
     "Accept-Language":"en-US,en;q=0.9",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
